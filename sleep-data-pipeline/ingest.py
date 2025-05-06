6afb30b15f5d5a999d5e46332d66a02a937827dc
Captain-jack0 <meryem.dilara788@gmail.com>
2025-05-06 12:19:23 +0300

    Çalışan versiyon

--- sleep-data-pipeline/ingest.py
+++ sleep-data-pipeline/ingest.py
@@ -1,13 +1,13 @@
-import os, json, time 
+import os, json, time
 import paho.mqtt.client as mqtt
 from paho.mqtt.client import CallbackAPIVersion
 
-BROKER_HOST = os.getenv("BROKER_HOST", "192.168.1.55")    #MQTT adresi
+BROKER_HOST = os.getenv("BROKER_HOST", "192.168.1.37")
 BROKER_PORT = int(os.getenv("BROKER_PORT", "1883"))
-TOPIC       = "sensors/+/data"  # tüm sensör topic’leri için wildcard
+TOPIC       = os.getenv("TOPIC", "sensors/+/data")
 
-def on_connect(client, userdata, flags, rc):
-    print(f"Bağlandı, sonuç kodu: {rc}")
+def on_connect(client, userdata, flags, rc, properties=None):
+    print(f"[Connected] rc={rc}")
     client.subscribe(TOPIC)
 
 def on_message(client, userdata, msg):
@@ -15,16 +15,23 @@ def on_message(client, userdata, msg):
         payload = json.loads(msg.payload)
         print(f"[{msg.topic}] => {payload}")
     except json.JSONDecodeError:
-        print(f"Geçersiz JSON: {msg.payload}")
+        print(f"Invalid JSON: {msg.payload}")
 
 def main():
-    client = mqtt.Client()
-    client = mqtt.Client(callback_api_version=CallbackAPIVersion.VERSION2)    
+    client = mqtt.Client(callback_api_version=CallbackAPIVersion.VERSION2)
     client.on_connect = on_connect
     client.on_message = on_message
-    client.connect(BROKER_HOST, BROKER_PORT, keepalive=60)
+
+    while True:
+        try:
+            print(f"Connecting to {BROKER_HOST}:{BROKER_PORT}…")
+            client.connect(BROKER_HOST, BROKER_PORT, keepalive=60)
+            break
+        except Exception as e:
+            print(f"Connection error: {e}. Retry in 5s")
+            time.sleep(5)
+
     client.loop_forever()
 
 if __name__ == "__main__":
     main()
-
--- sleep-data-pipeline/venv/bin/activate.fish
+++ sleep-data-pipeline/venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/merye/sleep-data-pipeline/venv"
+set -gx VIRTUAL_ENV "/Users/merye/mqtt-methods/sleep-data-pipeline/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
